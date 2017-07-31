a8fb0d3a6947e9f122494f2b3812b980e3326478
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-31 10:06:54 -0500

    added route resolution

--- app/app.js
+++ app/app.js
@@ -3,6 +3,19 @@
 let todoApp = angular.module("TodoApp", ["ngRoute"])
 .constant("FirebaseUrl", "https://todos-2430e.firebaseio.com/");
 
+let isAuth = (UserFactory) => {
+  return new Promise( (resolve, reject) => {
+    UserFactory.isAuthenticated()
+    .then( (userBoolean) => {
+      if(userBoolean) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  });
+};
+
 todoApp.config( ($routeProvider) => {
   $routeProvider
   .when('/', {
@@ -11,19 +24,23 @@ todoApp.config( ($routeProvider) => {
   })
   .when('/todos/view', {
     templateUrl: 'partials/todo-list.html',
-    controller: 'TodoListController'
+    controller: 'TodoListController',
+    resolve: {isAuth}
   })
   .when('/todos/new', {
     templateUrl: 'partials/todo-form.html',
-    controller: 'TodoAddController'
+    controller: 'TodoAddController',
+    resolve: {isAuth}
   })
   .when('/todos/detail/:todoId', {
     templateUrl: 'partials/todo-detail.html',
-    controller: 'TodoDetailController'
+    controller: 'TodoDetailController',
+    resolve: {isAuth}
   })
   .when('/todos/edit/:todoId', {
     templateUrl: 'partials/todo-form.html',
-    controller: 'TodoEditController'
+    controller: 'TodoEditController',
+    resolve: {isAuth}
   })
   .otherwise('/');
 });
